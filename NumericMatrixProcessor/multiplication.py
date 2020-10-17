1ab8e51606917dfc999d9ba1ab1010ff7539b299
mkarpik <marcin.karpik@gmail.com>
2020-10-16 21:06:43 -0700

    Stage 2 completed

--- NumericMatrixProcessor/multiplication.py
+++ NumericMatrixProcessor/multiplication.py
@@ -1,5 +1,16 @@
 def read_matrix(s_input):
+    l_matrix = []
+    lines = s_input.split('\n')
+    i_rows_a = int(lines[0].split(' ')[0])
 
+    i = 1
+    while i <= i_rows_a:
+        l_matrix.append(list(map(int, lines[i].split(' '))))
+        i += 1
+    return l_matrix
+
+
+def read_matrices(s_input):
     l_matrix_a = []
     l_matrix_b = []
     lines = s_input.split('\n')
@@ -18,6 +29,7 @@ def read_matrix(s_input):
         r += 1
 
     return l_matrix_a, l_matrix_b
+
 
 def matrix_addition(l_a, l_b):
     l_result = list()
@@ -38,35 +50,45 @@ def matrix_addition(l_a, l_b):
 
     return l_result
 
-def main():
 
+def matrix_multiplication_constant(l_a, constant):
+    l_result = list()
+    l_result = list()
+    #i_rows_a = len(l_a)
+
+    for i in l_a:
+        l_row_result =[]
+        for item in i:
+            l_row_result.append(int(item) * constant)
+        l_result.append(l_row_result)
+
+    return l_result
+
+
+def main():
     temp = input()
     s_input = temp + '\n'
     i_rows_a = int(temp[0].split(' ')[0])
 
     for l in range(i_rows_a):
         temp = input()
-        s_input += temp + '\n'
-
-    temp = input()
-    s_input += temp + '\n'
-    i_rows_a = int(temp[0].split(' ')[0])
-
-    for l in range(i_rows_a):
-        temp = input()
-        if l < i_rows_a:
+        if l < i_rows_a-1:
             s_input += temp + '\n'
         else:
             s_input += temp
 
-    l_matrix_a, l_matrix_b = read_matrix(s_input)
-    l_result = matrix_addition(l_matrix_a, l_matrix_b)
+    constant = int(input())
+    # s_input += temp
+    # i_rows_a = int(temp[0].split(' ')[0])
+    l_matrix_a = read_matrix(s_input)
+    l_result = matrix_multiplication_constant(l_matrix_a, constant)
 
     for row in l_result:
         str_ = ''
         for num in row:
             str_ += ' ' + str(num)
         print(str_.strip())
+
 
 if __name__ == "__main__":
     main()
