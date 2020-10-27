5be504a4965f581d50350c5243429b24a2ad6197
mkarpik <marcin.karpik@gmail.com>
2020-10-27 13:39:14 -0700

    Skeleton for new project

---
+++ TicTacToeWithAI/initialSetup.py
@@ -0,0 +1,14 @@
+def read_matrix(s_input):
+
+    l_temp = s_input.split()
+
+
+
+def main():
+    s_input = input()
+
+    l_matrix = read_matrix(s_input)
+
+
+if __name__ == "__main__":
+    main()
