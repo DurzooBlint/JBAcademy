1f5b1cc717e8c6d0125944af11cdfa2efc7e5ce8
mkarpik <marcin.karpik@gmail.com>
2020-11-03 18:59:57 -0800

    Chapter complete

--- Test.py
+++ Test.py
@@ -1,14 +1,2 @@
-text = [["Glitch", "is", "a", "minor", "problem", "that", "causes", "a", "temporary", "setback"],
-        ["Ephemeral", "lasts", "one", "day", "only"],
-        ["Accolade", "is", "an", "expression", "of", "praise"]]
-my_list = []
-
-n = 1
-
-for el in text:
-    for l in el:
-        if len(l) <= n:
-            my_list.append(l)
-
-
-print(my_list)
+prime_numbers = []
+all(x for in range(2, 1000, 1) if x % )
